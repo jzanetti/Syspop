c9ca517ff0f487ec4432905ae83e3dafe464757a
jzanetti <zsjzyhzp@gmail.com>
2024-02-06 09:34:36 +1300

    version changes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="syspop",
-    version="0.1.1",
+    version="0.1.2",
     setup_requires=["setuptools-git-versioning"],
     packages=find_packages(),
     install_requires=[
