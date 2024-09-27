3fd5b6d708c89717da03892663c9de5438213e61
sijin <zsjzyhzp@gmail.com>
2024-09-27 18:13:27 +1200

    add geopandas

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="syspop",
-    version="0.2",
+    version="0.2.1",
     setup_requires=["setuptools-git-versioning"],
     packages=find_packages(),
     install_requires=[
@@ -20,6 +20,7 @@ setup(
         "OSMPythonTools",
         "folium",
         "seaborn",
+        "geopandas"
     ],
     entry_points={
         "console_scripts": [
