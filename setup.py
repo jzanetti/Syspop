069785ff9ad92ed3ffadd05ed51786e0f2811d0e
sijin <zsjzyhzp@gmail.com>
2024-09-27 17:58:42 +1200

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="syspop",
-    version="0.1.9",
+    version="0.2",
     setup_requires=["setuptools-git-versioning"],
     packages=find_packages(),
     install_requires=[
